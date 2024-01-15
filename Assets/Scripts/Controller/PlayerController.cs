34debbedc67a6317551f73de8fa7238adb837277
Jakkrapan Thongphet <jakkrapan.art@gmail.com>
2024-01-15 15:39:07 +0700

    Add entity controll system

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : EntityController
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  public override Vector2 GetMoveInput()
+  {
+    return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+  }
 }
--- Assets/Scripts/Entity/Entity.cs
+++ Assets/Scripts/Entity/Entity.cs
@@ -7,6 +7,8 @@ public class Entity : MonoBehaviour
 {
   private Rigidbody2D _rb;
   private EntityController _controller;
+  [SerializeField]
+  private EntityData _data;
 
   #region Unity Functions
   private void Awake()
@@ -14,10 +16,17 @@ public class Entity : MonoBehaviour
     _rb = GetComponent<Rigidbody2D>();
   }
 
+  private void Start()
+  {
+    _controller = new PlayerController();
+  }
+
   private void Update()
   {
     if(_controller != null)
     {
+      Move(_controller.GetMoveInput());
+
       foreach(var action in _controller.KeyActions)
       {
         if (Input.GetKeyDown(action.Key)) action.Value?.Invoke();
@@ -28,7 +37,7 @@ public class Entity : MonoBehaviour
 
   public void Move(Vector2 direction)
   {
-    if(_rb) _rb.velocity = direction;
+    if(_rb) _rb.velocity = direction * _data.MoveSpeed * Time.fixedDeltaTime;
   }
 
   public void StopMove()
