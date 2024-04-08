b1380b2f3066dfe37d90c32f518d1a00d154ecb5
jakkrapan-art <57596151+jakkrapan-art@users.noreply.github.com>
2024-04-08 17:06:13 +0000

    Deploying to main from @ jakkrapan-art/bossrush2024@52a55c1e3124194eadad8a5f32984aec1a93b688 🚀

--- Assets/Scripts/Entity/EntityData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "EntityData", menuName = "Data/Entity")]
-public class EntityData : ScriptableObject
-{
-  public float MoveSpeed = 325;
-}
