c27b1b6306039bfaec75f2ec2ca92820228de973
Jakkrapan Thongphet <jakkrapan.art@gmail.com>
2024-05-08 01:27:59 +0700

    update boss max plant count data

---
+++ Assets/Scripts/Ability/AbilityEffects/GazeAbility.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Ability/Gaze", fileName = "Gaze")]
+public class GazeAbility : Ability
+{
+  public override void Setup()
+  {
+    base.Setup();
+  }
+  public override void Activate()
+  {
+    base.Activate();
+  }
+}
