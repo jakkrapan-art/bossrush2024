046978d387e57f41fe36ca97daf9ed27e2eb62ef
jakkrapan-art <57596151+jakkrapan-art@users.noreply.github.com>
2024-05-07 19:48:55 +0000

    Deploying to gh-pages from @ jakkrapan-art/bossrush2024@de068d15c9d33e1f759bc1dd5d15209b1fd8b050 🚀

--- WebGL/ServiceWorker.js
+++ WebGL/ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-bossrush2024-0.0.207";
+const cacheName = "DefaultCompany-bossrush2024-0.0.208";
 const contentToCache = [
     "Build/WebGL.loader.js",
     "Build/WebGL.framework.js",
